fa4bbbe78b68d1886307acf0689e58f3cd7d59ce
Dmytro Cisar <dmytro.cisar@gmail.com>
2021-09-07 01:47:30 +0300

    no-ts, windicss.config

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import Vue from "vue";
-import VueRouter, { RouteConfig } from "vue-router";
+import VueRouter from "vue-router";
 import Home from "@/views/Home.vue";
 import About from "@/views/About.vue";
 import NotFound from "@/views/NotFound.vue";
