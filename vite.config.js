3ebc419bbf179556746a2a012d28af85887155c6
Dmytro Cisar <dmytro.cisar@gmail.com>
2021-09-11 22:21:09 +0300

    minor change

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import path from "path";
 import { defineConfig } from "vite";
 import { createVuePlugin } from "vite-plugin-vue2";
-import Components from "unplugin-vue-components/vite";
 import WindiCSS from "vite-plugin-windicss";
+import Components from "unplugin-vue-components/vite";
 import Icons from "unplugin-icons/vite";
 import IconsResolver from "unplugin-icons/resolver";
 
